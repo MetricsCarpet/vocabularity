4985b42c0e7b943a7f0054bae2ba648c1e7218f6
Yauhen Yakimovich <yauhen.yakimovich@pricehubble.com>
2017-04-04 19:17:38 +0200

    Switched to pytest.

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -1,20 +1,24 @@
-import unittest
+"""
+Basic vocabularity metric tests.
+
+Usage (from the project root):
+
+    pytest
+
+"""
 from vocabularity import TypeZeroMetric
 
 
-class BasicTests(unittest.TestCase):
-    '''Test basic metrics'''
-
-    def test_metrics_type0(self):
-        metric = TypeZeroMetric()
-        filename = __file__.replace('.pyc', '.py')
-        metric.parse_words(open(filename))
-        # print(metric.words)
-        assert metric.words['zero'] == 3
-        metric.measure()
-        # print(metric.vocab_words)
-        # print(metric.results)
-        # Update the test?
-        assert metric.results['num_of_unique_vocab_words'] == 30, \
-            "Actual num of unique vocab words: %d" % \
-            metric.results['num_of_unique_vocab_words']
+def test_metrics_type0(self):
+    metric = TypeZeroMetric()
+    filename = __file__.replace('.pyc', '.py')
+    metric.parse_words(open(filename))
+    # print(metric.words)
+    assert metric.words['zero'] == 3
+    metric.measure()
+    # print(metric.vocab_words)
+    # print(metric.results)
+    # Update the test?
+    assert metric.results['num_of_unique_vocab_words'] == 30, \
+        "Actual num of unique vocab words: %d" % \
+        metric.results['num_of_unique_vocab_words']
